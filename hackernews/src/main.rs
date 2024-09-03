986dc7d25b36cf3c78724950b204bbf703712ecd
hedon954 <171725713@qq.com>
2024-09-03 20:51:56 +0800

    feat: support load comments according to `use_context` and cache with `use_signal`

--- hackernews/src/main.rs
+++ hackernews/src/main.rs
@@ -1,11 +1,11 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use dioxus_logger::tracing::{info, Level};
 use hackernews::App;
+use tracing::{info, Level};
 
 fn main() {
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
-    info!("starting app");
+    info!("starting hackernews app");
     launch(App);
 }
--- hackernews/src/ui/mod.rs
+++ hackernews/src/ui/mod.rs
@@ -2,48 +2,29 @@
 
 use dioxus::prelude::*;
 
-use self::story_item::StoryItem;
-use crate::api::get_top_stories;
+use self::{comments::Comments, stories::Stories};
+use crate::StoryData;
 
-mod story_comment;
-mod story_item;
+mod comments;
+mod stories;
+
+#[derive(Debug, Clone)]
+pub enum CommentsState {
+    Unset,
+    Loading,
+    Loaded(StoryData),
+}
 
 pub fn App() -> Element {
+    use_context_provider(|| Signal::new(CommentsState::Unset));
     rsx! {
         main { class: "flex w-full h-full shadow-lg rounded-3xl",
             section { class: "flex flex-col pt-3 w-4/12 bg-gray-50 h-full overflow-y-scroll",
                 Stories{}
             }
             section { class: "w-8/12 px-4 flex flex-col bg-white rounded-r-3xl",
-                section {
-                    ul {}
-                }
+                Comments{}
             }
         }
-    }
-}
-
-#[component]
-fn Stories() -> Element {
-    let stories = use_resource(move || get_top_stories(20));
-    match &*stories.read_unchecked() {
-        Some(Ok(stories)) => rsx! {
-            ul {
-                for story in stories {
-                    StoryItem {story: story.clone()}
-                }
-            }
-        },
-        Some(Err(err)) => rsx! {
-          div { class: "mt-6 text-red-500",
-            p { "Failed to fetch stories" }
-            p { "{err}" }
-          }
-        },
-        None => rsx! {
-          div { class: "mt-6",
-            p { "Loading stories..." }
-          }
-        },
     }
 }
